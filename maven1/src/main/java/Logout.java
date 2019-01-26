b67ebf10ca74d1b9d3f08b4b791b8a75dd85751a
charanroy <charanroy@CharanRoyal>
2019-01-26 23:36:53 +0530

    push

---
+++ maven1/src/main/java/Logout.java
@@ -0,0 +1,16 @@
+
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class Logout {
+WebDriver driver;
+ By logout=By.linkText("Log Out");
+public Logout(WebDriver driver) {
+	super();
+	this.driver = driver;
+}
+public void clickOnLogout() {
+	driver.findElement(logout).click();
+}
+}
